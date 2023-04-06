9b0322909c7a9af7c7d9acc15e956dd16671ee90
Roy <iRoySwift@iCloud.com>
2023-04-06 11:23:02 +0800

    ✨ new store

--- packages/example/src/store/states/app.ts
+++
@@ -1,7 +0,0 @@
-const initApp: State.App = {
-    appWidth: window.innerWidth,
-    appHeight: window.innerHeight,
-    language: navigator.language.includes("zh") ? "zh" : "en"
-};
-
-export default initApp;
