fe878483a9cbb506087fa4fb981885b94227f409
Roy <iRoySwift@iCloud.com>
2024-03-25 15:18:42 +0800

    fix

--- packages/next-example/src/layout/MainLayout/Drawer/index.tsx
+++ packages/next-example/src/layout/MainLayout/Drawer/index.tsx
@@ -9,7 +9,7 @@ const Drawer: React.FC<Props> = ({ drawer }) => {
     return (
         <nav className="z-50">
             <div
-                className={`fixed h-full  bg-primary-foreground opacity-80 ${drawer ? "w-[260px] border-r border-border" : "w-[60px] shadow"}`}>
+                className={`fixed h-full  bg-primary-foreground ${drawer ? "w-[260px] border-r border-border" : "w-[60px] shadow"}`}>
                 <LogoSelection drawer={drawer} />
                 <Content drawer={drawer} />
             </div>
--- packages/next-example/src/layout/MainLayout/Header/index.tsx
+++ packages/next-example/src/layout/MainLayout/Header/index.tsx
@@ -15,7 +15,7 @@ const Header: React.FC<Props> = ({ drawer }) => {
     const { openDrawer, closeDrawer } = useDrawerStore();
     return (
         <header
-            className={`fixed z-50 flex h-[60px] border-b border-border bg-primary-foreground opacity-80  ${drawer ? "left-[260px] w-[calc(100%-260px)]" : "left-[60px] w-[calc(100%-60px)]"}`}>
+            className={`fixed z-50 flex h-[60px] border-b border-border bg-primary-foreground ${drawer ? "left-[260px] w-[calc(100%-260px)]" : "left-[60px] w-[calc(100%-60px)]"}`}>
             <div className="flex w-full items-center justify-between p-2">
                 <div>
                     <Button variant="outline" size="icon">
